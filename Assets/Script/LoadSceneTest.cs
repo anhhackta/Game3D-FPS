5fec89c98104725a3517a6e3972995dcea402c17
Zon_206XLag <56264968+anhhackta@users.noreply.github.com>
2025-04-26 20:55:36 +0700

    Update 26/4

---
+++ Assets/Script/LoadSceneTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadSceneTest : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void LoadingScene()
+    {
+         SceneManager.LoadScene("SampleScene");
+    }
+}
